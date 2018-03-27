ab29b59ce38df1df48c92900ad74fbc46d08513d
ChipCheap <chip_44@hotmail.de>
2018-03-27 17:36:58 +0200

    init uml diagram

--- src/Game.java
+++ src/Game.java
@@ -1,2 +1,32 @@
+import java.util.ArrayList;
+
+/**
+ * Represents the entire game and controls its flow
+ */
 public class Game {
+    //fields
+    /**
+     * The player playing the game
+     */
+    private Player player;
+    /**
+     * The world map
+     */
+    private Map map;
+    /**
+     * The list of all existing ports
+     */
+    private ArrayList<Port> ports;
+
+    //constructors
+    /**
+     *
+     */
+    public Game(){
+        player = new Player();
+        map = new Map();
+        ports = new ArrayList<>();
+    }
+
+    //methods
 }
--- src/Player.java
+++ src/Player.java
@@ -1,2 +1,25 @@
+import java.util.ArrayList;
+
+/**
+ * Represents the player controlling the ships
+ */
 public class Player {
+    //fields
+    /**
+     *
+     */
+    private int money;
+    /**
+     *
+     */
+    private ArrayList<Ship> ships;
+
+    //constructors
+    /**
+     *
+     */
+    public Player(){
+        money = 300;
+    }
+    //methods
 }
