343d241a0e75e948e6523307a05a7c4b28d11818
Pattarek <Patrick.Nickel@hotmail.de>
2018-03-27 18:36:03 +0200

    Ship and Ware model updated

--- src/model/Player.java
+++ src/model/Player.java
@@ -3,16 +3,16 @@ package model;
 import java.util.ArrayList;
 
 /**
- * Represents the player controlling the ships
+ * Represents the player controlling the ships.
  */
 public class Player {
     //fields
     /**
-     *
+     *The amount of money the player has.
      */
     private int money;
     /**
-     *
+     *The ships, the player owns.
      */
     private ArrayList<Ship> ships;
 
